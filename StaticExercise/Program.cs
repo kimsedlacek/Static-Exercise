1d7b8725c73605acb7d4f13b2a0fba035be17f1d
Kim Sedlacek <sedlaceks@optonline.net>
2023-12-21 13:48:47 -0500

    static exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,12 @@
 ﻿namespace StaticExercise
 {
-    public class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"The Tempertaure in Celsius is {TempConverter.FahrenheitToCelsius(50)} degrees.");
+
+            Console.WriteLine($"The Temperature in Fahrenheit is {TempConverter.CelsiusToFahrenheit(30)} degrees.");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+       
+        public static double FahrenheitToCelsius(double fctemp)
+        {
+            return  (fctemp - 32) * (5.0/9.0);
+        }
+
+        public static double CelsiusToFahrenheit(double cftemp)
+        {
+            return (cftemp * (9.0/5.0)) + 32;
+        }
+    }
+}
